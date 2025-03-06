f7a910cb916b129278afaf16b222d1ff725173d1
GRafikjanov <gayratrafikjanov@gmail.com>
2025-03-06 12:05:13 +0500

    nothing changed

--- blog/views.py
+++ blog/views.py
@@ -34,7 +34,7 @@ all_posts = [
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
         "content": """
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          aperiam est praesentium, quos iste consequntur omnis exercitationem quam
           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
