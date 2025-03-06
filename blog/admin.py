7b4eb470929f49f81a3bfa8c1b10693bd1f44701
GRafikjanov <gayratrafikjanov@gmail.com>
2025-03-06 12:36:35 +0500

    Added PostAdmin class for costumize admin interface: filter, slugging.

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Author, Post, Tag   # import the models from models.py
 # Register your models here.
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "date", "author")
+    list_filter = ('author', 'date', 'tags')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
-admin.site.register(Post)
 admin.site.register(Tag)
